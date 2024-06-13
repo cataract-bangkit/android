09aad595eaddb6171326ad118fba90e9e594a03c
Joselin Permata Aprillia <103546691+joselinprmt@users.noreply.github.com>
2024-06-13 16:01:07 +0700

    feat: add date localization for history page, adjust files to be ignored in .gitignore

--- app/src/main/java/com/cataract/detection/connection/adapter/ListHistoryAdapter.kt
+++ app/src/main/java/com/cataract/detection/connection/adapter/ListHistoryAdapter.kt
@@ -1,10 +1,15 @@
 package com.cataract.detection.connection.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.cataract.detection.connection.model.ListHistoryModel
 import com.cataract.detection.databinding.CardHistoryBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 import kotlin.math.roundToInt
 
 class ListHistoryAdapter(private val dataArray: ArrayList<ListHistoryModel>) : RecyclerView.Adapter<ListHistoryAdapter.ListViewHolder>() {
@@ -22,7 +27,7 @@ class ListHistoryAdapter(private val dataArray: ArrayList<ListHistoryModel>) : R
         var persen = persenToDecimal?.let { toPercentage(it) }
 
 
-        holder.binding.itemDate.text            = item_predictedAt
+        holder.binding.itemDate.text            = item_predictedAt?.withDateFormat()
         holder.binding.itemResult.text          = item_result
         holder.binding.itemPersentance.text     = persen
 
@@ -32,13 +37,29 @@ class ListHistoryAdapter(private val dataArray: ArrayList<ListHistoryModel>) : R
         }
     }
 
-    fun stringToDouble(value: String): Double {
+    private fun stringToDouble(value: String): Double {
         return value.toDouble()
     }
 
-    fun toPercentage(value: Double): String {
+    private fun toPercentage(value: Double): String {
         val percentage = value * 100
         return "${percentage.roundToInt()}%"
+    }
+
+    private fun String.withDateFormat(): String? {
+        return try {
+            val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US)
+            inputFormat.timeZone = TimeZone.getTimeZone("UTC")
+
+            val date = inputFormat.parse(this) as Date
+            val outputFormat = SimpleDateFormat("dd MMMM yyyy, HH:mm", Locale("in", "ID"))
+
+            outputFormat.format(date)
+
+        } catch (e: Exception) {
+            Log.e("ListHistoryAdapter", e.toString())
+            this
+        }
     }
 
     override fun getItemCount(): Int = dataArray.size
